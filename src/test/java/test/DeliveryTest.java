4c1049f644c2092e4dd7361bd7d4c271d8282950
TregubovAV <5595@rambler.ru>
2025-04-08 12:15:56 +0700

    Обновлены тесты и артефакты, реализован тест на перепланирование встречи

---
+++ src/test/java/test/DeliveryTest.java
@@ -0,0 +1,60 @@
+package test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+import org.openqa.selenium.Keys;
+
+public class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.headless = false;
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSuccessfullyReplanMeetingWithAutocompleteAndCalendar() {
+        var user = DataGenerator.Registration.generateUser("ru");
+        var firstMeetingDate = DataGenerator.generateDate(4);
+        var secondMeetingDate = DataGenerator.generateDate(5);
+
+        // Город через автозаполнение
+        $("[data-test-id=city] input").setValue(user.getCity().substring(0, 3));
+        $$(".menu-item__control").find(Condition.exactText(user.getCity())).click();
+
+        // Первая дата через календарь
+        $("[data-test-id=date] input").click();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+
+        $("[data-test-id=name] input").setValue(user.getName());
+        $("[data-test-id=phone] input").setValue(user.getPhone());
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+
+        // Проверка первого успешного планирования
+        $("[data-test-id=success-notification]")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15));
+        $("[data-test-id=success-notification] button").click();
+
+        // Ожидание, чтобы UI успел отрисоваться
+        sleep(2000); // ожидание, чтобы UI успел отрисоваться
+
+        // Вторая дата
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+
+        $$("button").find(Condition.exactText("Запланировать")).click();
+
+        $("[data-test-id=replan-notification]")
+                .shouldHave(Condition.text("Необходимо подтверждение"), Duration.ofSeconds(15));
+        $$("button").find(Condition.exactText("Перепланировать")).click();
+    }
+}
